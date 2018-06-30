4dc74de2e53dd64d7eae412accdc7992065245d0
Your Name <you@example.com>
2018-06-30 14:30:48 +0530

    How to play complete

--- src/pages/how-to-play/how-to-play.ts
+++ src/pages/how-to-play/how-to-play.ts
@@ -7,16 +7,41 @@ import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angul
   templateUrl: 'how-to-play.html',
 })
 export class HowToPlayPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams ,public viewCtrl:ViewController) {
+  public value;
+  constructor(public navCtrl: NavController, public navParams: NavParams, public viewCtrl: ViewController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad HowToPlayPage');
   }
 
-  ionViewWillEnter(){
+  ionViewWillEnter() {
     this.viewCtrl.showBackButton(true);
   }
+  INTRODUCTION() {
+    this.value = "introduction";
+  }
+  into() {
+    this.value = 'introduction !=introduction'
+  }
 
+  CREATING() {
+    this.value = "creating";
+  }
+  crea() {
+    this.value = 'creating !=creating'
+  }
+  MAINDING() {
+    this.value = 'mainting';
+  }
+  main() {
+    this.value = 'mainting !=mainting'
+  }
+
+  FAQs() {
+    this.value="faqs";
+  }
+  faq() {
+    this.value = 'faqs !=faqs'
+  }
 }
